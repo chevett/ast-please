a96285f8c8b0e07ebb40797802bb44be6e394347
Mike Chevett <mchevett@appnexus.com>
2014-01-12 21:14:16 -0500

    more

--- browser/main.js
+++ browser/main.js
@@ -1,7 +1,25 @@
 var d3 = require('d3');
+var ast = global.model;
+
 
 d3.select('body')
-	.data(window.model.body)
+	.transition()
+	.duration(750)
+	.style('background-color', 'black');
+d3.select('body')
+	.data(ast.body)
 	.enter()
 	.append('div')
-	.text(function(d){return d.type; });
+	.transition()
+	.duration(750)
+	.delay(function(d, i) { return i * 200; })
+	.style('color', function(d){
+		switch (d.type){
+			case 'VariableDeclaration':
+				return 'cyan';
+
+			default:
+				return 'white';
+		}
+	})
+.text(function(d){return d.type ; });
--- index.js
+++ index.js
@@ -1,19 +1,41 @@
 var vash = require('vash'),
 	esprima = require('esprima'),
+	esCodeGen = require('escodegen'),
 	fs = require('fs'),
+	m = require('merle'),
 	concatStream = require('concat-stream'),
-	browserify = require('browserify'),
-	b = browserify(),
-	pageTemplate = vash.compile(fs.readFileSync('./page.vash').toString());
+	browserify = require('browserify');
 
-var fileContents = fs.readFileSync(process.argv[2]);
+function getScriptBundle(cb){
+	var b = browserify();
+	b.add(__dirname + '/browser/main.js');
+	b.bundle().pipe(concatStream(function(bundle){
+		cb(bundle);
+	}));
+}
 
-b.add(__dirname + '/browser/main.js');
-b.bundle().pipe(concatStream(function(bundle){
+function getDataForFile(file){
+	var fileContents = fs.readFileSync(file),
+	ast = esprima.parse(fileContents);
+
+	m(ast, function(){
+		if (!this.value || !this.value.type) return;
+		
+		this.value.code = esCodeGen.generate(this.value);
+	});
+
+	return ast;
+}
+
+var strPageTemplate = fs.readFileSync('./page.vash').toString();
+var pageTemplate = vash.compile(strPageTemplate);
+var data = getDataForFile(process.argv[2]);
+
+getScriptBundle(function(bundle){
 	var model = {
-		ast: JSON.stringify(esprima.parse(fileContents)),
+		ast: JSON.stringify(data),
 		bundle: bundle
 	};
 
 	process.stdout.write(pageTemplate(model));
-}));
+});
